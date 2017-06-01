618166be30c5951020a4fabd0ee6961fcc284108
Tayron Miranda <dev@tayron.com.br>
2017-06-01 11:11:50 -0300

    Update Exception.php
    Removendo exibição da mensagem de erro, para que fique a cargo da aplicação exibir a mensagem caso a exceção não seja tratada

--- Exception.php
+++ Exception.php
@@ -3,8 +3,6 @@ namespace Tayron\exceptions;
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-use Tayron\Request;
-use Tayron\Template;
 
 /**
  * Classe de Exceção que gera log das exceções lançadas
@@ -28,7 +26,6 @@ class Exception extends \Exception
         parent::__construct($message, 0, null);
         
         $this->writeLog($message, $trace);
-        $this->showErroMessage($message, $trace);
     }
     
     /**
@@ -45,36 +42,10 @@ class Exception extends \Exception
     {
         $listaParametroObjeto = explode('\\', get_class($this));        
         $nomeExcecao = end($listaParametroObjeto);
-        $trace = ($trace) ? $this->getTrace() : array();	
+        $traceMessage = ($trace) ? $this->getTrace() : array();	
 		
         $logger = new Logger($nomeExcecao);
         $logger->pushHandler(new StreamHandler("logs/{$nomeExcecao}.log", Logger::WARNING));
-        $logger->addError(strip_tags(str_replace('<br />', ' | ', $message)), $trace);         
-    }
-    
-    /**
-     * Exception::showErroMessage
-     * 
-     * Exibe mensagem de erro para o usuario
-     * 
-     * @param string $message Mensagem personalizada de erro
-     * @param string $trace Ativa ou desativa a exibiçaõ de trance do erro
-     * 
-     * @return void
-     */
-    private function showErroMessage($message, $trace = true)
-    {		
-        $message .= ($trace) ? '<br /><br /><pre>' . $this->getTraceAsString() . '</pre>' : null;
-        $request = Request::getInstance();
-        
-        $pathView = PATH . DS . 'src' . DS . 'view';
-        $pathTemplate = PATH . DS . 'src' . DS . 'view' . DS . 'template';
-        $pathElements = PATH . DS . 'src' . DS . 'view' . DS . 'elements';  
-        
-        $this->template = Template::getInstance(null, $request, $pathView, $pathTemplate, $pathElements);
-        
-        $this->template->setTemplate('error');
-        $this->template->setParameters(array('mensagem' => $message ));        
-        $this->template->render('exceptions/error');
+        $logger->addError(strip_tags(str_replace('<br />', ' | ', $message)), $traceMessage);         
     }
 }
